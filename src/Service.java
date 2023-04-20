4d2bdeb3799fc2322ddfb8ea38563bd2318518ae
d.tkakishak <Tkakishakd@mail.ru>
2023-04-20 17:15:12 +0300

    problem2

--- src/Service.java
+++ src/Service.java
@@ -2,11 +2,14 @@ public class Service {
 
 
     public void calculate(Gryffindor[] gryffindors) {
-        if (gryffindors != null){
-            for (Gryffindor gryffindor: gryffindors) {
-                int power = gryffindor.getCourage()+ gryffindor.getHonor()+ gryffindor.getNobility()+ gryffindor.getConjure()+ gryffindor.getTransgress();
+        if (gryffindors != null) {
+            int power = 0;
+            for (Gryffindor gryffindor : gryffindors) {
+                power = gryffindor.getCourage() + gryffindor.getHonor() + gryffindor.getNobility() + gryffindor.getConjure()
+                        + gryffindor.getTransgress();
                 System.out.println(power);
             }
+
         }
 
     }
